c1e8303fbf15c2d1796f4ecd55e229166f0dc088
kong yong sun <zeminage@gmail.com>
2016-12-19 16:26:11 +0900

    Add zeminage second

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
-i = 0
-sum = 0
+i = 0;
+sum = 0;
 while True:
+{
     i += 1
     if i == 5:
         continue
     if i > 10:
         break
     sum += i
- 
-print(sum)
-print( sum+2)
-print(sum*100)
+ }
+print(sum);;
+print(sum+2);
+print(sum*100);
+print('haha');
